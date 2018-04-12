b5921eb7a53f20d7a21c3d88eb52c5c30e85a466
Joseph Pillatsch <Joe@joes-mac-3.attlocal.net>
2018-04-11 22:32:12 -0500

    Add NSP basic site + prior html5up site by palmierie

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# nashville-sound-panels
-Nashville Sound Panels Website
+# This is the NSP Website Github
+
+* managed by Joe & Eric 
+
+* and sometimes Jon
+
+## To work on this project
+* clone this repo
+* from the command line, run ```npm install``` in the lib directory
+> this will install all dependencies
+* from the command line, run ```grunt``` in the lib directory
+> this will render the index.html file and watch for changes to the index.pug file
+* make edits to the index.pug file located at ```assets/pug/```
+* don't burn things in the toaster oven
---
+++ assets/js/new_main.js
@@ -0,0 +1,33 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // FullPage.js
+  $(document).ready(function() {
+    $("#fullpage").fullpage({
+      recordHistory: "false",
+      //Navigation
+      menu: "menu",
+      anchors: [],
+      //Design
+      controlArrows: true,
+      verticalCentered: true
+    });
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll-trigger").click(function() {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: 54
+  });
+
+  // Email
+  var usrnme = "team";
+  var hstnme = "nashvillesoundpanels.com";
+  var linktext = "mail" + "to:" + usrnme + "@" + hstnme;
+  $("#eml").attr("href", linktext);
+})(jQuery); // End of use strict
