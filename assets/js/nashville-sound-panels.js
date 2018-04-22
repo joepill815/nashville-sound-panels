7abb8db6843420134cfd0a7fcb0bdfd6311caf03
Joe Pillatsch <nashvillesoundpanels@gmail.com>
2018-04-21 19:52:23 -0500

    Made the favicon on top more circular, removed the cursive from the logo on the first page, put the cursive in the top left

--- assets/js/nashville-sound-panels.js
+++ assets/js/nashville-sound-panels.js
@@ -51,4 +51,20 @@ var feed = new Instafeed({
     });
   }
 });
+// grab out load more button
+var loadButton = document.getElementById('load-more');
+var feed = new Instafeed({
+  // every time we load more, run this function
+  after: function() {
+    // disable button if no more results to load
+    if (!this.hasNext()) {
+      loadButton.setAttribute('disabled', 'disabled');
+    }
+  },
+});
+
+// bind the load more button
+loadButton.addEventListener('click', function() {
+  feed.next();
+});
 feed.run();
